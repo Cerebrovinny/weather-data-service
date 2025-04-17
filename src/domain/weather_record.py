a5e7ee3a88ede8669746c6d04e7cf537e0afebab
Cerebrovinny <vinny.cardoso96@gmail.com>
2025-04-17 14:05:34 +0100

    domain entity implementation

--- src/domain/weather_record.py
+++ src/domain/weather_record.py
@@ -1,3 +1,9 @@
 # Domain entity: WeatherRecord
 class WeatherRecord:
-    pass
+    def __init__(self, city, temperature, timestamp):
+        if not isinstance(temperature, (float, int)):
+            raise TypeError("Temperature must be a float or int.")
+        self.city = city
+        self.temperature = temperature
+        self.timestamp = timestamp
+
